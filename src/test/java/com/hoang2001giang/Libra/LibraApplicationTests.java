90ac6606c0c327da59fc01102bed5ee7756b1842
Phạm Hoàng Giang <sprigan0902@gmail.com>
2023-12-13 00:12:32 +0700

    first commit

---
+++ src/test/java/com/hoang2001giang/Libra/LibraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoang2001giang.Libra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
